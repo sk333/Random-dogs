fbf090a4c835b51ff75aa8374433006b7e18278f
Shiyao Zhang <shiyao.zhang1@student.unimelb.edu.au>
2020-03-01 12:30:33 +1100

    The app can display images and mp4 correctly both on PC and mobile devices.

--- random-dogs/src/App.js
+++ random-dogs/src/App.js
@@ -7,111 +7,132 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            list: [],
-            img: null,
-            baseUrl: 'https://random.dog/'    //这里创建urls保存接口的路径，后面img标签调用图片用到
+            list1: [],
+            list2: [],
+            type1: [],
+            baseUrl: 'https://random.dog/woof.json',    //这里创建urls保存接口的路径，后面img标签调用图片用到
+            availableTypes: ["mp4", "MP4", "JPG", "jpg", "gif", "GIF", "JPEG", "jpeg", "png", "PNG"],
+            test: ""
         }
     }
 
     getApiData = () => {
-        let api = this.state.baseUrl + "woof.json";
-        let {list, img, bu} = this.state;
-        fetch(api)
-            .then(response => response.json())
-            .then(data => data.url)
-            // .then(img => this.setState({img:img}))
-            .then(img => this.setState({
-                list: [...list, img]
-            }))
-            .catch(e => e)
+        let api = this.state.baseUrl;
+        for (var i = 0; i < 4; i++) {
+            // fetch(api)
+            //     .then(response => response.json())
+            //     .then(data => data.url)
+            //     .then(function(url)  {
+            //         this.setState({
+            //             test: "keyidaozhelio",
+            //         });
+            //         let type = "";
+            //         let index = 0;
+            //         let judgement = true;
+            //         index = url.lastIndexOf(".");
+            //         type  = url.substring(index + 1, url.length);
+            //         if(this.state.availableTypes.includes(type)){
+            //
+            //             return {type:url, b:500, color:"white"}[url,true];
+            //         }
+            //         return [url,false];
+            //     })
+            //     .then(function(result) { // (**)
+            //             if(result[1]){
+            //                 this.setState({
+            //                     list1: this.state.list1.concat([result[0]]),
+            //                     test: result
+            //                 })
+            //             }
+            //             else{
+            //                 i--;
+            //             }
+            //         }
+            //     )
+            fetch(api)
+                .then(response => response.json())
+                .then(data => data.url)
+                .then(img => this.setState({
+                    list1: this.state.list1.concat([img]),
+                }))
+        }
+
+        for (var i = 0; i < 4; i++) {
+            fetch(api)
+                .then(response => response.json())
+                .then(data => data.url)
+                .then(img => this.setState({
+                    list2: this.state.list2.concat([img]),
+                }))
+        }
+    }
+
+
+    getType(s){
+        let type = "";
+        let index = 0;
+        let judgement = true;
+        index = s.lastIndexOf(".");
+        type  = s.substring(index + 1, s.length);
+        if(this.state.availableTypes.includes(type)){
+            return true;
+        }
+        return false;
     }
 
     componentDidMount(){
-        for (var i=0;i<2;i++){
-            this.getApiData();
-        }
+        this.getApiData();
     }
-    // render() {
-    //     return (
-    //         <div className="home">
-    //           <div className="item">
-    //               {
-    //                   //一级渲染
-    //                   this.state.list.map((item, index) => {
-    //                       return (
-    //                           <div className="list" key={index}>
-    //                             <h5>{item.title}</h5>
-    //                             <ul>
-    //                                 {
-    //                                     // 二级渲染
-    //                                     item.list.map((lis, index) => {
-    //                                         return (
-    //                                             <li key={index}>
-    //                                               <Link to={`/Dateils/${lis._id}`}>
-    //                                                 <p>{lis.title}</p>
-    //                                                 <p style={{color:"red"}}>{lis.price}元</p>
-    //                                               </Link>
-    //                                             </li>
-    //                                         )
-    //                                     })
-    //                                 }
-    //
-    //                             </ul>
-    //
-    //                           </div>
-    //                       )
-    //                   })
-    //               }
-    //           </div>
-    //         </div>
-    //     )
-    // }
 
     render() {
-
-      return (<div class="container">
+        let _this = this;
+        let list1 = this.state.list1.map((item) => (
+            (item.search("mp4") == -1 ? (
+                <div class="col-md-3 column">
+                    <img src={item} class="img-thumbnail" />
+                </div>)
+                    :
+                    (<div class="col-md-3 column">
+                        <iframe class="embed-responsive-item" src={item} width="100%"></iframe>
+                    </div>)
+        )
+        ));
+        let list2 = this.state.list2.map((item) => (
+            (item.search("mp4") == -1 ? (
+                        <div class="col-md-3 column">
+                            <img src={item} class="img-thumbnail" />
+                        </div>)
+                    :
+                    (<div class="col-md-3 column">
+                        <iframe class="embed-responsive-item" src={item} width="100%"></iframe>
+                    </div>)
+            )
+        ));
+        return (
             <div class="row clearfix">
               <div class="col-md-12 column">
                 <div class="row clearfix">
                   <div class="col-md-12 column">
                     <div class="row clearfix">
-                      <div class="col-md-3 column">
-                        <img src={this.state.list[0]} class="img-thumbnail"/>
-                        {/*<img src="test_images/cat.JPG" class="img-thumbnail" />*/}
-                      </div>
-                      <div class="col-md-3 column">
-                        <img src={this.state.list[1]} class="img-thumbnail" />
-                      </div>
-                      <div class="col-md-3 column">
-                        <img src="test_images/cat.JPG" class="img-thumbnail" />
-                      </div>
-                      <div class="col-md-3 column">
-                        <img src="test_images/cat.JPG" class="img-thumbnail" />
-                      </div>
+                        <h1 id="hereiam">{this.state.test}</h1>
+                        {
+                            list1
+                            }
                     </div>
                   </div>
                 </div>
+                  <br></br>
                 <div class="row clearfix">
                   <div class="col-md-12 column">
                     <div class="row clearfix">
-                      <div class="col-md-3 column">
-                        <img src="test_images/cat.JPG" class="img-thumbnail" />
-                      </div>
-                      <div class="col-md-3 column">
-                        <img src="test_images/cat.JPG" class="img-thumbnail" />
-                      </div>
-                      <div class="col-md-3 column">
-                        <img src="test_images/cat.JPG" class="img-thumbnail" />
-                      </div>
-                      <div class="col-md-3 column">
-                        <img src="test_images/cat.JPG" class="img-thumbnail" />
+                      {
+                          list2
+                      }
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
       )
     }
 }
